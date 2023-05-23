ffb2a985c5ee8e54db084246b6f6dfe46e69381a
Cristian Aguirre <cristianmoto066@gmail.com>
2023-05-23 18:29:18 -0300

    flatlist agregada

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,44 +1,60 @@
-import { StyleSheet, TouchableOpacity, View, FlatList, Image } from 'react-native'
-import React from 'react'
-import data from '../data/data';
-import Items from '../components/Items';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { StyleSheet,Button,  TouchableOpacity, Text, View,  FlatList,  Image,} from "react-native";
+import React from "react";
+import data from "../data/data";
+import Items from "../components/Items";
+import { useState } from "react";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Tab = createMaterialTopTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
-  );
-}
 
 const MenuScreen = () => {
-  return (
- <View>
-    <Image style={styles.image} source={require("../screens/img/coffee.webp")} />
-    
-  
-  <FlatList 
- 
-       data={data}
-       renderItem={({item:props})=>(
-           <Items {...props}/>
-       )}
-       />
-      
-        </View>   
-   );
-}
+  const [visibleItems, setVisibleItems] = useState(5);
+  const [myData, setMyData] = useState(data.slice(0, visibleItems));
 
-export default MenuScreen
+  const cargarMasElementos = () => {
+    setVisibleItems(prevVisibleItems => prevVisibleItems + 5);
+    setMyData(data.slice(0, visibleItems + 5));
+  };
+
+  return (
+    <View style={styles.container} > 
+    <Image
+      style={styles.image}
+      source={require("../screens/img/coffee.webp")}
+    />
+      <FlatList
+        data={myData}
+        renderItem={({ item: props }) => <Items {...props} />}
+          keyExtractor={(Items, index) => index.toString()}
+      />
+     
+      <TouchableOpacity onPress={cargarMasElementos} style={styles.button}>
+                        <MaterialCommunityIcons name="plus" color={"#fff"} size={30} />
+              </TouchableOpacity>
+    </View>
+  );
+
+
+}
+export default MenuScreen;
+
 
 const styles = StyleSheet.create({
   image: {
-    width:'100%',
-    height:'20%',
-  
+    width: "100%",
+    height: "20%",
   },
+  container: {
+    marginBottom: 30,
+    marginHorizontal:5,
+ 
+  },
+button:{
+  width: 40,
+  height: 40,
+  backgroundColor:"#451717",
+  alignItems: 'center',
+  justifyContent: 'center',
+  marginLeft:"45%",
+  borderRadius:25,
+}
 })
